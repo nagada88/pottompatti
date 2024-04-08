978069a076e84880c9df89b32a5b61c22a8ec8e4
Nagy Ádám <nagada88@gmail.com>
2024-04-08 23:37:56 +0200

    settings

--- pottompatti/settings.py
+++ pottompatti/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
-SECRET_KEY = config('SECRET_KEY')
 
 # Application definition
 
