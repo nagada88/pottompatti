d48654baee040d6c6274f565fca30600b38193ca
Nagy Ádám <nagada88@gmail.com>
2024-04-16 21:05:48 +0200

    python decoupling bugfixes

--- pottompatti/settings.py
+++ pottompatti/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
